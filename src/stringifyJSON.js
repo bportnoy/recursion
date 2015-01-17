bb298f909edec5512cc39dae07b28a2c7554fdae
Bradley Portnoy <bradley.portnoy@gmail.com>
2015-01-16 23:10:14 -0800

    Create function to handle base case primitives.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,22 +5,52 @@
 
 var stringifyJSON = function(obj) {
   var result = "";
-  if 
   
-
-
-  result = (stack === null) ? result : stringifyJSON(result);
+  //result = (stack === null) ? result : stringifyJSON(result);
 
   return result;
 };
 
 function stringifyPrimitive(input){
-
+  switch (typeof input){
+  	case 'number':
+  	  return input.toString();
+  	  break;
+  	case 'boolean':
+  	  return input.toString();
+  	  break;
+  	case 'string':
+  	  return '\"' + input + '\"';
+  	  break;
+  	case 'object':
+  	  if (_.isArray(input)) return stringifyArray(input);
+  	  else return stringifyObject(input);
+  	  break;
+  	default:
+  	console.log("Invalid input: " + input);
+  }
 }
 
 function stringifyArray(array){
-	var string = "[";
-	string.concat(array.toString());
-	string.concat("]");
+	var string = "[" + array + "]";
+	//string.concat(array.toString());
+	//string.concat("]");
 	return string;
 }
+
+function stringifyObject(object){
+	var string = "";
+	return string;
+}
+
+var testobj = {x:5,y:"string"};
+var testarr = [1,3,"hello"];
+
+console.log(testobj);
+console.log(stringifyArray(testarr));
+console.log(JSON.stringify(testarr));
+console.log("test");
+console.log(JSON.stringify("test"));
+console.log(stringifyPrimitive("test"));
+console.log(true.toString());
+console.log(JSON.stringify(true));
